aefe7dbfd2ca07c5f0b39cd5fedc25ce583651aa
Pic Aurélia <pic.aurelia@gmail.com>
2025-02-26 11:43:59 +0100

    Add TP01 & TP02 solutions

---
+++ TP01.js
@@ -0,0 +1,21 @@
+const num1 = 1;
+const num2 = 2;
+
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num1 % num2);
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 == num2);
+console.log(num1 != num2);
+
+const b1 = true;
+const b2 = false;
+
+console.log(b1 && b2);
+console.log(b1 || b2);
+console.log(!b1);
+console.log(!b2);
